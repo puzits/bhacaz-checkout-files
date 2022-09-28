ae939ba7544e9d09fb9d555b167d4b1e97ac612a
puzits <puzitsingh@outlook.com>
2022-09-28 13:37:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const token = core.getInput('token');
 const octokit = github.getOctokit(token)
 
 const files = core.getInput('files', { required: true }).split(' ');
-let repository =  core.getInput('respository') || 'Icertis-Engineering/ici-devops-deployment';
+let repository =  core.getInput('respository');
 
 const owner = repository.split('/')[0]
 const repo = repository.split('/')[1]
